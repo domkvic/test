3fd9f059716be2c1c6d66e2176d6cc1aec4da231
khuongdomediastep <49667984+khuongdomediastep@users.noreply.github.com>
2020-03-06 15:23:22 +0700

    add-config-path

--- delete.ps1
+++ delete.ps1
@@ -2,11 +2,13 @@ $branchName = $args[0]
 $currentTime = $('{0:MMddyyyy}' -f (Get-Date))
 
 Get-Content "$PSScriptRoot\config" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+$gitPath = $h.Get_Item("GitLocalPath")
 $PERIOD = $h.Get_Item("Period")
 $REMOTE_NAME = $h.Get_Item("RemoteName")
 Write-Host "Running Config Period: $PERIOD"
 Write-Host "==================================="
 
+cd $gitPath
 
 if ([string]::IsNullOrWhitespace($(git log -1 --since=$PERIOD "$REMOTE_NAME/$branchName")))
 {	
--- list.ps1
+++ list.ps1
@@ -1,9 +1,12 @@
 Get-Content "$PSScriptRoot\config" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+$gitPath = $h.Get_Item("GitLocalPath")
 $PERIOD = $h.Get_Item("Period")
 Write-Host "Running Config Period: $PERIOD"
 Write-Host "==================================="
-
 $currentTime = $('{0:MMddyyyy}{0:HHmmss}' -f (Get-Date))
+
+cd $gitPath
+
 ForEach ($k in $(git branch -r))
 {
 	Write-Host "Running branch $k"
